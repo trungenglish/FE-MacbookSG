6fe29d1b0c339f42a41d02aa1e99df684e4eb23d
trungenglish <nguyentrung23232323@gmail.com>
2024-11-08 22:47:09 +0700

    add avalibity product

--- src/components/Product/ProductList.jsx
+++ src/components/Product/ProductList.jsx
@@ -53,7 +53,7 @@ const ProductList = () => {
                 </div>
                 :
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                    {products.map((product) => (
+                    {products.filter((product) => product.isActive).map((product) => (
                         <ProductCard key={product._id} product={product}/>
                     ))}
                 </div>
