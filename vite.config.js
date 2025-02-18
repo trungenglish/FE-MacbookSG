22b720c72f3cd16a1d9f9fe8e723472718096454
trungenglish <nguyentrung23232323@gmail.com>
2025-02-18 23:22:27 +0700

    fix ssl for server

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: true
+  },
   optimizeDeps: {
     exclude: ['axios'],
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
